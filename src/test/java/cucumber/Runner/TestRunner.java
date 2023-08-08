86ec35d77261767ca7556e140c9eb487a5461412
muthukumaran.qa@gmail.com <muthukumaran.qa@gmail.com>
2023-08-08 15:17:42 +0100

    Test for commit

--- src/test/java/cucumber/Runner/TestRunner.java
+++ src/test/java/cucumber/Runner/TestRunner.java
@@ -4,11 +4,13 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-	features = "src/test/java/cucumber/Feature/Login.feature",
+	features = "src/test/java/cucumber/Feature/Login.featuress",
 	glue = "cucumber.Testdefinition",
 	dryRun = false,
 	plugin = {"pretty"}
+	
 
+	
 		)
 
 public class TestRunner extends AbstractTestNGCucumberTests{
